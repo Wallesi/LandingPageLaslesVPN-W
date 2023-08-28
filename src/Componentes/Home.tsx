072b4c87377dc3ba7a3d6c69c5146bce1ff46cd1
wallesi <waldemargalizzi98@gmail.com>
2023-08-28 12:02:46 -0300

    Probando 2

--- src/Componentes/Home.tsx
+++ src/Componentes/Home.tsx
@@ -1,3 +1,4 @@
+import home from "dist/assets/home.svg";
 
 const Home = () => {
     return (
@@ -10,7 +11,7 @@ const Home = () => {
                 </div>
 
                 <div className="w-full md:w-1/2 p-4">
-                    <img className="max-w-full" src="dist/assets/home.svg" alt="Imagen de humano con notebook" />
+                    <img className="max-w-full" src={home} alt="Imagen de humano con notebook" />
                 </div>
             </div>
 
